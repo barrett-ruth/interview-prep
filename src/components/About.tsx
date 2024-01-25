9e24edfdb80a57261f4b301437d754511204c177
Barrett Ruth <br.barrettruth@gmail.com>
2024-01-25 18:34:00 -0500

    fix(about): grammar

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -23,19 +23,18 @@ function About() {
         Consequently, I will be extensively practicing on platforms like
         LeetCode, HackerRank, and TopCoder to enhance my algorithmic thinking.
         The effectiveness of these platforms as metrics for an individual's
-        thought process is debatable, but one thing is certain—they often
-        receive unjust criticism.
+        thought process is debatable, but one thing is certain&mdash;they get a
+        bad wrap.
       </p>
       <p>
         This editorial serves as both a problem-solving guide and a record of my
         progress.
       </p>
       <p>
-        I do not consider myself especially talented in data structures and
-        algorithms. However, I believe that people often overcomplicate
-        problems. Through this editorial, I aim to offer alternative
-        perspectives and new insights to facilitate easier, quicker, and more
-        effective learning.
+        I do not consider myself talented in data structures and algorithms.
+        However, I believe that people often overcomplicate problems. I hope
+        this editorial can present alternative perspectives and new intuitions
+        to faciliate easier, quicker, and deeper learning.
       </p>
     </>
   )
